59cef0e86a5f23c5f774a4c7428ede841ff903bf
Felipe Soares <felipesoarwz@gmail.com>
2024-09-30 22:37:33 -0300

    round 28 updated

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
 
   useEffect(() => {
     atualizarClassificacao(rounds);
-    const specificRound = 28; // índice da rodada que deve focar
+    const specificRound = 29; // índice da rodada que deve focar
     setActiveRoundNumber(specificRound - 1);
   }, []);
 
